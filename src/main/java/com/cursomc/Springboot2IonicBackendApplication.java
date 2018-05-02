a4b542e022de0ebc4654bae27cfcbfc369bb912f
Philip Mes <fmesy@outlook.com>
2018-05-02 00:24:16 -0300

    first commit

---
+++ src/main/java/com/cursomc/Springboot2IonicBackendApplication.java
@@ -0,0 +1,12 @@
+package com.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2IonicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2IonicBackendApplication.class, args);
+	}
+}
