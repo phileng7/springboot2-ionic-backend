545fa29c75de92e4935d559b434650a98af89f31
Philip Mes <fmesy@outlook.com>
2018-05-07 22:05:43 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/cursomc/services/PedidoService.java
+++ src/main/java/com/cursomc/services/PedidoService.java
@@ -37,6 +37,9 @@ public class PedidoService {
 	
 	@Autowired
 	private ClienteService clienteService;
+	
+	@Autowired
+	private EmailService emailService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -67,7 +70,8 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);		//vai chamar o toString do objeto
+		//System.out.println(obj);		//vai chamar o toString do objeto
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 }
