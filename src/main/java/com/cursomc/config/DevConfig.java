ab6f193d2df60e60557c3889efc60be17b2be65a
Philip Mes <fmesy@outlook.com>
2018-05-07 23:15:23 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/cursomc/config/DevConfig.java
+++ src/main/java/com/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.cursomc.services.DBService;
+import com.cursomc.services.EmailService;
+import com.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -17,7 +19,7 @@ public class DevConfig {
 	@Autowired
 	DBService dbService;
 	
-	@Value("spring.jpa.hibernate.ddl-auto")
+	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
 
 	@Bean
@@ -27,5 +29,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/cursomc/services/DBService.java
+++ src/main/java/com/cursomc/services/DBService.java
@@ -111,7 +111,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null,"Felipe Mes","fmesy@outlook.com","36378912377",TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("23763323","93838393"));
 		
 		Endereco e1 = new Endereco(null,"Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, cid1);
