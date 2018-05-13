d17bd57f4c4d7eb7cd5b45015b6ce92517310a49
Philip Mes <fmesy@outlook.com>
2018-05-13 00:15:23 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/cursomc/Springboot2IonicBackendApplication.java
+++ src/main/java/com/cursomc/Springboot2IonicBackendApplication.java
@@ -1,38 +1,17 @@
 package com.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cursomc.domain.Categoria;
-import com.cursomc.domain.Cidade;
-import com.cursomc.domain.Cliente;
-import com.cursomc.domain.Endereco;
-import com.cursomc.domain.Estado;
-import com.cursomc.domain.ItemPedido;
-import com.cursomc.domain.Pagamento;
-import com.cursomc.domain.PagamentoComBoleto;
-import com.cursomc.domain.PagamentoComCartao;
-import com.cursomc.domain.Pedido;
-import com.cursomc.domain.Produto;
-import com.cursomc.domain.enums.EstadoPagamento;
-import com.cursomc.domain.enums.TipoCliente;
-import com.cursomc.repositories.CategoriaRepository;
-import com.cursomc.repositories.CidadeRepository;
-import com.cursomc.repositories.ClienteRepository;
-import com.cursomc.repositories.EnderecoRepository;
-import com.cursomc.repositories.EstadoRepository;
-import com.cursomc.repositories.ItemPedidoRepository;
-import com.cursomc.repositories.PagamentoRepository;
-import com.cursomc.repositories.PedidoRepository;
-import com.cursomc.repositories.ProdutoRepository;
+import com.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class Springboot2IonicBackendApplication implements CommandLineRunner{
+	
+	@Autowired
+	S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2IonicBackendApplication.class, args);
@@ -40,6 +19,6 @@ public class Springboot2IonicBackendApplication implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("foto.jpg","E:\\tempFig\\104_1.jpg");
 	}
 }
