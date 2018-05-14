5be1bb1aff5c804c10d7e2841c8011a2458abdd8
Philip Mes <fmesy@outlook.com>
2018-05-13 23:04:06 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/cursomc/services/ClienteService.java
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -47,6 +48,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -123,7 +130,7 @@ public class ClienteService {
 		if (user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+		/*
 		URI uri = s3Service.uploadFile(multipartFile);		//upload da imagem e retorna URI
 		
 		Optional<Cliente> cli = repo.findById(user.getId());
@@ -131,15 +138,14 @@ public class ClienteService {
 		repo.save(cli.get());
 		
 		return uri;
+		*/
 		
-		/*
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
-		jpgImage = imageService.cropSquare(jpgImage);
-		jpgImage = imageService.resize(jpgImage, size);
+		//jpgImage = imageService.cropSquare(jpgImage);
+		//jpgImage = imageService.resize(jpgImage, size);
 		
 		String fileName = prefix + user.getId() + ".jpg";
 		
-		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
-		*/
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");		
 	}
 }
